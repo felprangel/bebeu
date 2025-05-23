e4db6634f22c20485b90c5c2011205b1043e8593
felprangel <feliperangel897@gmail.com>
2025-05-23 19:42:23 -0300

    adiciona estilização

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,19 @@
+import { colors } from "@/assets/styles/colors";
 import Logo from "@/assets/svg/logo.svg";
+import { Text, TextInput, TouchableHighlight, View } from "react-native";
 import { styled } from "styled-components/native";
 
 export default function Index() {
   return (
     <CenterView>
       <Logo />
+      <View style={{ marginTop: 40, width: 300, height: 400 }}>
+        <TextInput placeholder="Email" />
+        <TextInput placeholder="Senha" />
+        <TouchableHighlight style={{ backgroundColor: colors.primary }}>
+          <Text style={{ color: colors.text.contrast }}>Login</Text>
+        </TouchableHighlight>
+      </View>
     </CenterView>
   );
 }
