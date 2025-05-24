fe3d627a0125ed34fd0fe990c1777532315fc842
felprangel <feliperangel897@gmail.com>
2025-05-23 21:03:56 -0300

    adiciona carregamento das fontes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,22 @@
+import {
+  Fredoka_400Regular,
+  Fredoka_500Medium,
+  Fredoka_600SemiBold,
+  Fredoka_700Bold,
+  useFonts,
+} from "@expo-google-fonts/fredoka";
 import { Slot } from "expo-router";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Fredoka_400Regular,
+    Fredoka_500Medium,
+    Fredoka_600SemiBold,
+    Fredoka_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return;
+  }
   return <Slot />;
 }
