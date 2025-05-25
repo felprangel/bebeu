630b96b4811669d4efe81257b26085b468fef5b4
felprangel <feliperangel897@gmail.com>
2025-05-25 16:18:23 -0300

    adiciona elementos da tela de registro

--- app/register.tsx
+++ app/register.tsx
@@ -1,5 +1,47 @@
-import { Text } from 'react-native'
+import { colors } from '@/assets/styles/colors'
+import { fontFamily } from '@/assets/styles/font-family'
+import Lock from '@/assets/svg/lock-icon.svg'
+import User from '@/assets/svg/user-icon.svg'
+import { Button } from '@/components/Button'
+import { Input } from '@/components/Input'
+import { Text, View } from 'react-native'
 
 export default function Register() {
-  return <Text>Hello World</Text>
+  return (
+    <View
+      style={{
+        display: 'flex',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <View
+        style={{
+          marginTop: 40,
+          width: 300,
+          height: 400,
+          gap: 30,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Input placeholder="Email" autoComplete="email" icon={<User />} />
+        <Input placeholder="Senha" autoComplete="new-password" secureTextEntry icon={<Lock />} />
+        <Input placeholder="Confirme sua Senha" autoComplete="new-password" secureTextEntry icon={<Lock />} />
+        <Button>
+          <Text
+            style={{
+              color: colors.text.contrast,
+              fontFamily: fontFamily.medium,
+              fontSize: 20
+            }}
+          >
+            Registrar
+          </Text>
+        </Button>
+      </View>
+    </View>
+  )
 }
