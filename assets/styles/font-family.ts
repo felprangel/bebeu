c2eaa9703942c268359bc6a25918e3447dc401f1
felprangel <feliperangel897@gmail.com>
2025-05-25 09:39:35 -0300

    altera ordem das fontes

--- assets/styles/font-family.ts
+++ assets/styles/font-family.ts
@@ -1,6 +1,6 @@
 export const fontFamily = {
-  medium: "Fredoka_500Medium",
   regular: "Fredoka_400Regular",
+  medium: "Fredoka_500Medium",
   semiBold: "Fredoka_600SemiBold",
   bold: "Fredoka_700Bold",
 };
