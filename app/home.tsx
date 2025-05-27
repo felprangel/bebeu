656f7ecb14a1bd6c577f061ab298a407a62ada21
felprangel <feliperangel897@gmail.com>
2025-05-26 21:11:02 -0300

    adiciona logout de teste

--- app/home.tsx
+++ app/home.tsx
@@ -1,5 +1,16 @@
+import { Button } from '@/components/Button'
+import { useAuth } from '@/hooks/useAuth'
 import { Text } from 'react-native'
 
 export default function Home() {
-  return <Text>Hello World</Text>
+  const Auth = useAuth()
+
+  return (
+    <>
+      <Text>Hello World</Text>
+      <Button onPress={() => Auth.logout()}>
+        <Text>Teste</Text>
+      </Button>
+    </>
+  )
 }
