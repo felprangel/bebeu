972a220b80689321543d541d976f6680878b8615
felprangel <feliperangel897@gmail.com>
2025-05-26 21:43:13 -0300

    altera url

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: process.env.EXPO_PUBLIC_API_URL
+  // TODO: url estática temporáriamente, não consegui fazer os envs funcionarem
+  baseURL: 'https://bookmark-api.felpo.dev'
 })
