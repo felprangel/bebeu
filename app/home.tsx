91a14f2e466e048735fcd5e7409fd0b0c9ec7312
felprangel <feliperangel897@gmail.com>
2025-05-26 21:59:26 -0300

    adiciona envio para a nova tela

--- app/home.tsx
+++ app/home.tsx
@@ -1,16 +1,17 @@
 import { Button } from '@/components/Button'
 import { useAuth } from '@/hooks/useAuth'
+import { useRouter } from 'expo-router'
 import { Text } from 'react-native'
 
 export default function Home() {
   const Auth = useAuth()
+  const Router = useRouter()
 
   return (
     <>
       <Text>Hello World</Text>
-      <Button onPress={() => Auth.logout()}>
-        <Text>Teste</Text>
-      </Button>
+      <Button text="Teste" onPress={() => Auth.logout()} />
+      <Button text="Ir pra outra tela" onPress={() => Router.push('/weight')} />
     </>
   )
 }
