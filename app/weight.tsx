bc8f3507993323f978b6b7a8257db8c576e3f0cd
felprangel <feliperangel897@gmail.com>
2025-05-26 21:58:48 -0300

    adiciona tela de peso

---
+++ app/weight.tsx
@@ -0,0 +1,24 @@
+import { colors } from '@/assets/styles/colors'
+import { fontFamily } from '@/assets/styles/font-family'
+import { Button } from '@/components/Button'
+import { SafeAreaView, Text } from 'react-native'
+
+export default function Weight() {
+  return (
+    <SafeAreaView
+      style={{
+        display: 'flex',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 10,
+        borderColor: 'red'
+      }}
+    >
+      <Text style={{ color: colors.text.default, fontFamily: fontFamily.medium, fontSize: 30, paddingBottom: 30 }}>
+        Qual o seu peso atual?
+      </Text>
+      <Button text="Calcular a minha meta de água" onPress={() => console.log('teste')} />
+    </SafeAreaView>
+  )
+}
