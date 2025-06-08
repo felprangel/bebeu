5d60a786636eb4203d95018b41a4838783eef90b
felprangel <feliperangel897@gmail.com>
2025-06-08 12:04:50 -0300

    adiciona envio do token na request

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,19 @@
+import { TOKEN_KEY } from '@/database/local'
 import axios from 'axios'
+import * as SecureStore from 'expo-secure-store'
 
 export const api = axios.create({
   // TODO: url estática temporáriamente, não consegui fazer os envs funcionarem
   baseURL: 'https://bookmark-api.felpo.dev'
 })
+
+api.interceptors.request.use(
+  async request => {
+    const token = await SecureStore.getItemAsync(TOKEN_KEY)
+    if (token) {
+      request.headers.Authorization = `Bearer ${token}`
+    }
+    return request
+  },
+  error => Promise.reject(error)
+)
