1b411b99680efb63a623d62528b757860d980bc8
felprangel <feliperangel897@gmail.com>
2025-06-14 20:01:32 -0300

    adiciona input no modal

--- components/WaterGoalModal.tsx
+++ components/WaterGoalModal.tsx
@@ -1,17 +1,20 @@
 import { colors } from '@/assets/styles/colors'
 import { fontFamily } from '@/assets/styles/font-family'
 import BigCup from '@/assets/svg/big-cup.svg'
-import { Modal, Text, View } from 'react-native'
+import { useState } from 'react'
+import { Modal, Text, TextInput, View } from 'react-native'
 import { Button } from './Button'
 
 interface WaterGoalProps {
   visible: boolean
   goal: number
   loading: boolean
-  confirmationFn(): void
+  confirmationFn(goal: number): void
 }
 
 export function WaterGoalModal(props: WaterGoalProps) {
+  const [goalInput, setGoalInput] = useState<number>(props.goal)
+
   return (
     <Modal visible={props.visible}>
       <View
@@ -27,14 +30,29 @@ export function WaterGoalModal(props: WaterGoalProps) {
         </Text>
         <View style={{ alignItems: 'center', gap: 30 }}>
           <BigCup />
-          <Text style={{ color: colors.text.default, fontFamily: fontFamily.medium, fontSize: 30, paddingBottom: 30 }}>
-            {props.goal} ml
-          </Text>
+          <View style={{ flexDirection: 'row', alignItems: 'center', gap: 10 }}>
+            <TextInput
+              onChangeText={input => setGoalInput(parseInt(input))}
+              defaultValue={props.goal.toString()}
+              style={{
+                width: 150,
+                color: colors.text.default,
+                fontFamily: fontFamily.medium,
+                fontSize: 20,
+                paddingLeft: 20,
+                borderWidth: 1,
+                borderColor: colors.primary,
+                borderRadius: 10
+              }}
+              inputMode="numeric"
+            />
+            <Text style={{ color: colors.text.default, fontFamily: fontFamily.medium, fontSize: 25 }}>ml</Text>
+          </View>
         </View>
         <Button
           loading={props.loading}
           text="Vamos hidratar!"
-          onPress={props.confirmationFn}
+          onPress={() => props.confirmationFn(goalInput)}
           style={{ width: 250, paddingHorizontal: 40 }}
         />
       </View>
